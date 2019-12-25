2e8d8894a9c10d34fc785f55b10c2f64caa778b3
auliaramadhan <aul.dark@gmail.com>
2019-12-25 09:01:43 +0700

    mau di tes

--- src/route/cart.js
+++ src/route/cart.js
@@ -9,13 +9,28 @@ router.get('/', auth, (req, res) => {
    mysql.execute(sql,[id], sqlexec)
 })
 
-router.post('/putitem', auth, (req,res) =>{
+router.post('/putitemtocart', auth, (req,res) =>{
+   if (req.user.roles !== 'customer') {
+      re.send({success:false, msg:'langsung ambil aja di toko'})
+   }
    const id_user = req.user.id
-   const {id_item} = req.body
+   const {id_item, qty} = req.body
 
-   const sql = "INSERT INTO cart (id_user, id_item) VALUES (?,?)"
+   const sql = "INSERT INTO cart (id_user, id_item,qty) VALUES (?,?,?)"
 
-
+   mysql.execute(sql, [id_user, id_item, qty], sqlexec)
 
 })
 
+router.delete('/removefromcart/:id', auth, (req,res) =>{
+   if (req.user.roles !== 'customer') {
+      re.send({success:false, msg:'langsung ambil aja di toko'})
+   }
+   const id = req.params.id
+   // const id_user = req.user.id
+   // const {id_item, qty} = req.body
+
+   const sql = "DELETE cart where id=?"
+
+   mysql.execute(sql, [id_user, id_item, qty], sqlexec)
+})
